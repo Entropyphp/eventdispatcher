e2d22b83dd0a1dc0c32f196592767de202fffef2
willy <william.lety@gmail.com>
2025-06-20 18:53:02 +0200

    First commit

---
+++ tests/EventTest.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PgTest\Event;
+
+use Pg\Event\Event;
+use PHPUnit\Framework\TestCase;
+
+class EventTest extends TestCase
+{
+    public function testEventNameReturnsConstantValue(): void
+    {
+        $event = $this->getNewEvent();
+        $this->assertSame('custom.test.event', $event->eventName());
+    }
+
+    public function testEventNameIsString(): void
+    {
+        $event = $this->getNewEvent();
+        $this->assertIsString($event->eventName());
+    }
+
+    public function testEventNameIsNotModifiedAfterInstantiation(): void
+    {
+        $event = $this->getNewEvent();
+        $name1 = $event->eventName();
+        $name2 = $event->eventName();
+        $this->assertSame($name1, $name2);
+    }
+    protected function getNewEvent(): Event
+    {
+        return new class extends Event{
+            public const NAME = 'custom.test.event';
+        };
+    }
+}
